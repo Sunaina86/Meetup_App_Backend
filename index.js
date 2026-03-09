ad2f335c16e7ac40df33cf5983dcee7180c40b0f
Sunaina86 <sunainabhatia091@gmail.com>
2026-03-09 16:17:02 +0530

    fix: add frontend link

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ initializeDatabase()
 
 const cors = require("cors");
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://meetup-app-frontend-opal.vercel.app/",
   credentials: true,
   optionSuccessStatus: 200,
 };
